cfe76aaf265ec106d881615c707d871975890d74
Javiera Leyton <javi95lm@gmail.com>
2026-04-21 11:44:24 -0400

    Update productos.py

--- Proyecto Sistema de Ventas/productos.py
+++ Proyecto Sistema de Ventas/productos.py
@@ -4,7 +4,7 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1q2w3e4r",
+        password="password",
         database="ventas_db"
     )
 
